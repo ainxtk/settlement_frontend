ce1669f5119f0c693febf17b1fb4d06a196438eb
EC2 Default User <ec2-user@ip-172-31-1-51.us-east-2.compute.internal>
2020-06-18 18:59:08 +0000

    change

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,21 +11,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { JobModule }     from './pages/job/job.module'; 
+import {LoginComponent} from './pages/login/login.component';
+import {RegistrationComponent} from './pages/registration/registration.component';
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
+    LoginComponent,RegistrationComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    // JobModule,
     AppRoutingModule,
     IconsProviderModule,
     NgZorroAntdModule,
     FormsModule, ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule
+  ],
+  exports: [
+    LoginComponent, RegistrationComponent
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/pages/registration/registration.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'registration-form',
+  template: `
+    <form nz-form [formGroup]="validateForm" class="login-form" (ngSubmit)="submitForm()">
+      <nz-form-item>
+        <nz-form-control nzErrorTip="Please input your username!">
+          <nz-input-group nzPrefixIcon="user">
+            <input type="text" nz-input formControlName="userName" placeholder="Username" />
+          </nz-input-group>
+        </nz-form-control>
+      </nz-form-item>
+      <nz-form-item>
+        <nz-form-control nzErrorTip="Please input your email address!">
+          <nz-input-group nzPrefixIcon="mail">
+            <input type="email" nz-input formControlName="email" placeholder="Email Address" />
+          </nz-input-group>
+        </nz-form-control>
+      </nz-form-item>
+      <nz-form-item>
+        <nz-form-control nzErrorTip="Please input your Password!">
+          <nz-input-group nzPrefixIcon="lock">
+            <input type="password" nz-input formControlName="password" placeholder="Password" />
+          </nz-input-group>
+        </nz-form-control>
+      </nz-form-item>
+      
+      <button nz-button class="login-form-button login-form-margin" [nzType]="'primary'">Sign Up</button>
+    </form>
+  `,
+  styles: [
+    `
+      .login-form {
+        max-width: 300px;
+      }
+
+      .login-form-margin {
+        margin-bottom: 16px;
+      }
+
+      .login-form-forgot {
+        float: right;
+      }
+
+      .login-form-button {
+        width: 100%;
+      }
+    `
+  ]
+})
+export class RegistrationComponent implements OnInit {
+  validateForm!: FormGroup;
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      email: [null, [Validators.required]]
+    });
+  }
+}
